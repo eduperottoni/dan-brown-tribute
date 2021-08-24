2a7a0bfe063fd36101d0c087cd14b1c8edafd53a
eduperottoni <edu.perottoni@gmail.com>
2021-08-23 23:25:43 -0300

    Finalizando timeline, citação externa, e livros

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //ANIMAÇÃO NO UNDERLINE DO MENU
 const divUnderline = document.querySelectorAll('.underline')
 
+
 function turnOnUnderline(value){
     divUnderline[value].classList.add('on')
+    console.log(divUnderline[value])
 }
 
 function turnOffUnderline(value){
     divUnderline[value].classList.remove('on')
+    console.log(divUnderline[value])
 }
